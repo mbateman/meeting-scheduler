ef35d44384b679720e4137d7b1b27beda4c10fa1
Michael Bateman <michael.bateman.com@gmail.com>
2015-05-26 21:36:30 +0100

    refactored to for each

--- src/main/java/com/metability/bookings/BookingPrinter.java
+++ src/main/java/com/metability/bookings/BookingPrinter.java
@@ -2,28 +2,30 @@ package com.metability.bookings;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import static java.util.stream.Collectors.toList;
 
 public class BookingPrinter {
 
 	public static void printBookings(List<Booking> bookings) {
-		List<Booking> bookingsByStartDateTime = bookings
-				.stream()
-				.sorted((b1, b2) -> b1.getStartDateTime().compareTo(b2.getStartDateTime())).collect(toList());
-		String previousDate = "";
-		for (Booking booking : bookingsByStartDateTime) {
+		StringBuilder schedule = new StringBuilder();
+		bookings.stream()
+				.sorted((b1, b2) -> b1.getStartDateTime()
+				.compareTo(b2.getStartDateTime()))
+				.forEach(booking -> {
 			String date = booking.getStartDateTime().format(DateTimeFormatter.ISO_DATE);
-			if (!previousDate.equals(date)) {
-				System.out.println(date+"\n");
+			if (!schedule.toString().contains(date)) {
+				schedule.append(date+"\n\n");
 			}
-			System.out.println(
-				String.format("%02d:%02d %02d:%02d %s", 
-					booking.getStartDateTime().getHour(), 
-					booking.getStartDateTime().getMinute(), 
-					booking.getStartDateTime().plusHours(booking.getDuration()).getHour(), 
-					booking.getStartDateTime().plusHours(booking.getDuration()).getMinute(), 
-					booking.getEmployeeId())+"\n");
-			previousDate = date;
-		}
+			schedule.append(addMeeting(booking));
+		});
+		System.out.println(schedule.toString().substring(0, schedule.length()-1));
+	}
+
+	private static String addMeeting(Booking booking) {
+		return String.format("%02d:%02d %02d:%02d %s", 
+			booking.getStartDateTime().getHour(), 
+			booking.getStartDateTime().getMinute(), 
+			booking.getStartDateTime().plusHours(booking.getDuration()).getHour(), 
+			booking.getStartDateTime().plusHours(booking.getDuration()).getMinute(), 
+			booking.getEmployeeId()+"\n\n");
 	}
 }
